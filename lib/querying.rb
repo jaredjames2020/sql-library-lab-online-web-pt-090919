0f1565a06021b489a37b255df8f053456b01b8c3
Jared S <jaredjames2020@gmail.com>
2019-11-14 00:22:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series "
+  "SELECT series.title FROM series
+  LEFT OUTER JOIN characters ON series.author_id = characters.author_id WHERE species = 'human' GROUP BY series.title ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
